2288a157766c7ea1e0cc56f8e921a8765ed58f28
KaterynaDubinina <ekaterynadubinina@gmail.com>
2023-11-05 19:11:21 +0200

    Add app menu

--- src/app/App.java
+++ src/app/App.java
@@ -2,10 +2,34 @@ package app;
 
 import app.controller.AppController;
 
+import java.util.Scanner;
+
 public class App {
 
+    static Scanner scanner;
+
     public static void main(String[] args) {
-        AppController controller = new AppController();
-        controller.getMiles();
+        runConverter(getConverter());
     }
+
+    private static int getConverter(){
+        System.out.print("""
+                Press:
+                1 - If you want to convert miles to km.
+                2 - If you want to convert km to miles.
+                """);
+        scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    private static void runConverter(int converter) {
+        switch (converter) {
+            case 1 -> {
+                AppController controller = new AppController();
+                controller.getMiles();
+            }
+            default -> System.out.println("No such option");
+        }
+    }
+
 }
