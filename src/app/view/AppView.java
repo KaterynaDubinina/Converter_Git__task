93fd09e254cb933705219b1666beb5ab83a3028b
KaterynaDubinina <ekaterynadubinina@gmail.com>
2023-11-05 19:32:50 +0200

    Change app info: short text

--- src/app/view/AppView.java
+++ src/app/view/AppView.java
@@ -9,7 +9,7 @@ public class AppView {
     public Input getData() {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
-        System.out.print("Here you can convert miles to kilometers! \nEnter value: ");
+        System.out.print("Enter value to convert miles to km: ");
         double value = scanner.nextDouble();
         return new Input(value);
     }
